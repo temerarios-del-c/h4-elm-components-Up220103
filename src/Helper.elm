49493347ee2c8190810c364abe4fd2d0c5a473af
up220103 <up220103@alumnos.upa.edu.mx>
2026-02-22 19:30:07 -0600

    tarea 4 terminada

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,32 +1,42 @@
 module Helper exposing (..)
 
-import Html exposing (Html, div, h1, h2, h3, h4, h5, h6, a, text)
+import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
 import Html.Attributes exposing (href)
 
 
--- --- FUNCIONES LÓGICAS ---
 
--- 1. Une dos cadenas usando el operador (++)
+-- --- FUNCIONES LÓGICAS ---
+-- 1. Une dos cadenas usando el operador (++)ss
+
+
 joinWords : String -> String -> String
 joinWords word1 word2 =
     word1 ++ word2
 
 
+
 -- 2. Transforma cada Char de la lista a Bool usando Char.isUpper
+
+
 isUpperChars : List Char -> List Bool
 isUpperChars list =
     List.map Char.isUpper list
 
 
+
 -- 3. Función de orden superior que aplica una función de transformación a una lista
+
+
 evalChars : List Char -> (Char -> Bool) -> List Bool
 evalChars list funTrans =
     List.map funTrans list
 
 
--- --- COMPONENTES DE VISTA ---
 
+-- --- COMPONENTES DE VISTA ---
 -- 4. Genera un div con encabezados del h1 al h6
+
+
 headers : String -> Html msg
 headers param =
     div []
@@ -39,13 +49,18 @@ headers param =
         ]
 
 
+
 -- 5. Genera un hipervínculo <a> dado una URL y un texto
+
+
 hyperlink : String -> String -> Html msg
 hyperlink url textParam =
     a [ href url ] [ text textParam ]
 
 
+
 -- --- PUNTO DE ENTRADA ---
+
 
 main : Html msg
 main =
